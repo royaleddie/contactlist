b92d131e6797058207ba175dfba056cb39e7067a
royaleddie <iginuaendurance@gmail.com>
2023-07-14 05:01:54 +0100

    Update

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('addcontact/', views.addContact, name='addcontact')
+    path('addcontact/', views.addContact, name='addcontact'),
+    path('profile/<str:pk>', views.contactProfile, name='profile'),
+    path('editcontact/<str:pk>', views.editContact, name='editcontact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -16,7 +16,14 @@ def addContact(request):
             phone_number = request.POST['phone-number'],
             # address = request.POST['address'],
         )
-        
         new_contact.save()
         return redirect('/') 
     return render(request, 'new.html')
+
+def contactProfile(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'contact-profile.html', {'contact': contact})
+
+def editContact(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'edit.html', {'contact': contact})
